2522457cc9fa72dacf1ab22d1572994ce834d1f8
Ianrowl <iaro8747@colorado.edu>
2023-09-09 20:57:46 -0600

    Assignment 2

--- Assignment 2/ContentView.swift
+++ Assignment 2/ContentView.swift
@@ -1,19 +1,42 @@
 //
 //  ContentView.swift
-//  Assignment 2
+//  Hello, World
 //
-//  Created by Ian Rowland on 9/9/23.
+//  Created by Ian Rowland on 8/31/23.
 //
 
 import SwiftUI
+import AuthenticationServices
 
 struct ContentView: View {
+    @State var major: String = ""
+    @State var year: String = ""
+
+
     var body: some View {
         VStack {
+            
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
             Text("Hello, world!")
+            Divider()
+            VStack{
+                
+                TextField("Major", text: $major)
+                TextField("Grad Year", text: $year)
+            }
+            VStack{
+                Capsule()
+                    .foregroundColor(.teal)
+                
+                Text("Their major is \(major)")
+                    .padding()
+                Text("Their graduation year is \(year)")
+            }
+            Toggle(isOn: .constant(true)) {
+                Text("HI")
+            }
         }
         .padding()
     }
